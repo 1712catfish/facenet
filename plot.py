5c5ebe59a6c2219e306a49e7c7301ac6dcbe823d
tan-kun <1712huynhtan@gmail.com>
2022-06-23 17:32:38 +0700

    Build

--- plot.py
+++ plot.py
@@ -6,19 +6,20 @@ def show_plot(iteration, loss):
     plt.savefig('loss.png')
     plt.show()
 
+
 def plot_history(history):
     plt.plot(history['accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    # summarize history for loss
+    plt.show(block=True)
+
     plt.plot(history['loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    plt.show(block=True)
 
 # show_plot(counter, loss_history)
